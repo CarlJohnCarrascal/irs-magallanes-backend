31106d03fd944faf7d1891369d3feac8e7424ecf
cajsen <carrascalcarljohn1@gmail.com>
2023-04-25 20:35:12 +0800

    new con db

---
+++ app/Models/Notification.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'message',
+        'notif_id',
+        'isseen',
+        'seen_date',
+    ];
+}
